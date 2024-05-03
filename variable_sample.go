67858c5df6cb1cff570a8397b6ce32b42bd85019
leo-1228 <akiraito731@gmail.com>
2024-05-02 22:43:59 -0700

    package main

---
+++ variable_sample.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var student1 string = "John" //type is string
+	var student2 = "Jane"        //type is inferred
+	x := 2                       //type is inferred
+
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
+}
