0059926131da5fc773f1a8774e2308d93d96ca6a
leo-1228 <akiraito731@gmail.com>
2024-05-02 23:13:21 -0700

    add operators_sample code

---
+++ operators_sample.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var (
+		sum1 = 100 + 50    // 150 (100 + 50)
+		sum2 = sum1 + 250  // 400 (150 + 250)
+		sum3 = sum2 + sum2 // 800 (400 + 400)
+	)
+	fmt.Println(sum3)
+}
