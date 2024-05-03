cce1265563ed9fe279113223b6ae5ef47dac721a
leo-1228 <akiraito731@gmail.com>
2024-05-02 23:11:19 -0700

    add modify slice action to slice_sample code

--- slice_sample.go
+++ slice_sample.go
@@ -14,7 +14,8 @@ func main() {
 	fmt.Println(len(myslice2))
 	fmt.Println(cap(myslice2))
 	fmt.Println(myslice2)
-	arr1 := [6]int{10, 11, 12, 13, 14, 15}
+	arr1 := []int{10, 11, 12, 13, 14, 15}
+	arr1 = append(arr1, 20, 21, 22, 23)
 	myslice := arr1[2:4]
 
 	fmt.Printf("myslice = %v\n", myslice)
@@ -31,4 +32,12 @@ func main() {
 	fmt.Printf("myslice2 = %v\n", myslice21)
 	fmt.Printf("length = %d\n", len(myslice21))
 	fmt.Printf("capacity = %d\n", cap(myslice21))
+
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15}
+
+	neededNumbers := numbers[:len(numbers)-10]
+	numbersCopy := make([]int, len(neededNumbers))
+	copy(numbersCopy, neededNumbers)
+
+	fmt.Printf("numbersCopy = %v\n", numbersCopy)
 }
