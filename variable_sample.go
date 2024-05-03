daadbfedf1bea3088985e14d600ec6fcba7ef96f
leo-1228 <akiraito731@gmail.com>
2024-05-02 22:47:14 -0700

    add contant variable

--- variable_sample.go
+++ variable_sample.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 )
 
+const PI = 3.14
+
 func main() {
 	var student1 string = "John" //type is string
 	var student2 = "Jane"        //type is inferred
@@ -12,4 +14,5 @@ func main() {
 	fmt.Println(student1)
 	fmt.Println(student2)
 	fmt.Println(x)
+	fmt.Println(PI)
 }
